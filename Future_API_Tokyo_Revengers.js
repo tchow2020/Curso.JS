4a22b121a93653602d84069949f2b142122d8478
tchow2020 <lgmds2017@outlook.com>
2023-08-07 11:17:42 -0300

    Lista_1

---
+++ Future_API_Tokyo_Revengers.js
@@ -0,0 +1,50 @@
+const link_API = "https://tokyorevengers.fandom.com/wiki/Tenjiku"
+
+const Gangues_Tokyo = function(nome) {
+    switch (nome) {      
+        case('Toman'):{
+            console.log("A Gangue Tokyo Manji (東京卍會, Tōkyō Manji-Kai?), Frequentemente abreviada como Toman (東卍, Tōman?), é uma gangue de motoqueiros com sede em Shibuya, Tóquio. Era conhecido por seus ideais e crenças honrosas que contribuíram para seu objetivo de estabelecer uma nova era de ouro para os delinquentes na região de Kanto.[3] No entanto, essa imagem desmoronou à medida que suas fileiras foram continuamente corrompidas e transformadas em uma organização criminosa que tirou a vida de civis inocentes nos dias atuais.[4] Ao longo de seu reinado, Toman enfrentou várias gangues e declarou vitória contra elas, fazendo com que crescesse rapidamente em número até sua dissolução. Dois anos depois, Toman é reconstruído por Takemichi Hanagaki sob o título de Segunda Geração como um esforço para derrubar a gangue Kanto Manji.[5]")
+            break;
+        }
+            
+        case('Moebius'):{
+            console.log("Moebius (愛美愛主メビウス, Mebiusu?) Era uma gangue de motociclistas baseada em Shinjuku. Estava duas gerações acima da Tokyo Manji Gang e era reconhecida por sua reputação odiosa na cidade.[1] Depois de perder seu oitavo líder, Nobutaka Osanai, em uma batalha contra Toman, Moebius foi comandado por Shuji Hanma como seu líder temporário.[2] Com o Hanma no comando, Moebius se envolveu em outra batalha com a gangue, e mais tarde tornou-se sua afiliada ao grupo de 1990 comandado por seu outro líder oficial, Tetta Kisaki.")
+            break;
+        }
+
+        case("Valhalla"):{
+            console.log("Valhalla ( 芭流覇羅バルハラ, Baruhara?), Frequentemente estilizado como 'WALHALLA'[2][3] e ocasionalmente chamado de 'O Anjo Sem Cabeça',[4] era uma gangue de motociclistas formada anonimamente por Tetta Kisaki para abrir caminho para sua promoção como segundo em comando de Manjiro Sano no Tokyo Manji Gang. Junto com esse objetivo estava fazer de Mikey o líder de Valhalla, o que fortaleceria a base da gangue e transformaria Toman em uma poderosa organização criminosa. No comando estava Shuji Hanma, que serviu como líder interino de Valhalla para contrabalançar a ausência de Kisaki da gangue. Hanma liderou a gangue até sua dissolução e lealdade a Toman após o Halloween Sangrento.")
+            break;
+        }
+
+        case('Black Dragons'):{
+            console.log("Black Dragon (Black Dragon, Burakku Doragon?) é uma gangue de motociclistas de Tóquio que tem governado ativamente o Japão por gerações, estabelecida pela primeira vez por Shinichiro Sano. Em linha com a derrota da 10ª Geração em uma batalha contra o Tokyo Manji Gang, Black Dragon tornou-se um afiliado de Toman sob a Primeira Divisão.[3] Sua afiliação com Toman serviria mais tarde como um caminho para Takemichi Hanagaki estabelecer a 11ª Geração com Seishu Inui como seu braço direito.[4]")
+            break;
+        }
+
+        case('Tenjiku'):{
+            console.log("Tenjiku (bairro japonês, Tenjiku?), ou Yokohama Tenjiku (novo distrito sul-africano, Yokohama Tenjiku?), Era o maior grupo criminoso de Kanagawa baseado em Yokohama. Foi fundada por Izana Kurokawa com a ajuda de Tetta Kisaki e da Geração S-62 para se vingar de Manjiro Sano e assumir o controle da Tokyo Manji Gang. Após a batalha que resultou na morte de Izana e na prisão de S-62, Tenjiku declarou sua derrota e dissolução. No salto de tempo final, Tenjiku é reformado em uma organização internacional sem fins lucrativos para ajudar órfãos sem privilégios.")
+            break;
+        }
+
+        case('Boten'):{
+            console.log("Bonten (梵天ぼんてん, Bonten?) é uma organização criminosa liderada por Manjiro Sano como produto da vitória da gangue Kanto Manji na Batalha das Três Divindades. Bonten também é conhecido como o sindicato do crime mais temido do Japão.[1]")
+            break;
+        }
+
+        case('Kanto Manji'):{
+            console.log("A gangue Kanto Manji (関東卍會, Kantō Manji-Kai?) é uma gangue de motociclistas formada durante a era da Guerra Civil de Tóquio. Atualmente é liderado por Manjiro Sano. Com a conclusão da Batalha das Três Divindades que resultou na morte de South Terano, a Gangue Kanto Manji expande seus números ao assumir o controle de Rokuhara Tandai.[2]")
+            break;
+        }
+
+        case("Brahman"):{
+            console.log("Brahman foi formado por Senju Kawaragi no ano de 2006 com o objetivo de derrubar Manjiro Sano, o infame 'Invincible Mikey', da gangue Kanto Manji.[1] Senju procurou a ajuda de seu irmão mais velho, Takeomi Akashi, e seus colegas da Primeira Geração Black Dragon na criação da equipe, e concedeu-lhes altos cargos como seu conselheiro e principais executivos, respectivamente, uma vez que o Brahman foi estabelecido.[2] origens de Brahman De acordo com Ken Ryuguji, Brahman começou como um poderoso grupo de lutadores que dirigiam um ringue subterrâneo conhecido como 'B1'. Após algum tempo comandando o cartel, o B1 criou o Brahman, um temível grupo militante de combate que agora controla Shinjuku.[4] A formação e a prevalência de Brahman afetaram fortemente o resultado da Era da Guerra Civil de Tóquio, um período de confronto de dois anos entre as inúmeras gangues em Tóquio para preencher o vácuo de poder, no qual Brahman prevaleceu como uma das três gangues mais poderosas. Como resultado, Brahman manteve o equilíbrio de controle no mundo delinquente de Tóquio, ao lado da gangue Kanto Manji e Rokuhara Tandai.[5] No entanto, apesar de governar Tóquio com o mesmo poder das outras duas gangues, a administração de Brahman assumiu o objetivo inicial da organização, que era parar a gangue Kanto Manji, que eles viam de forma negativa em comparação com a agora dissolvida Tokyo Manji Gang. .[6]")
+            break;
+        }
+        default:
+            console.log("Gangue não encontrada");
+           
+    }   
+}
+
+Gangues_Tokyo('')
