f94c992f77bdacd6d3220283ac9fa8b28dad8069
tchow2020 <lgmds2017@outlook.com>
2023-09-04 09:49:17 -0300

    Initial-commit

---
+++ Atividades/Atividade_5.js
@@ -0,0 +1,11 @@
+//  Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
+
+function maiorOuIgual(num_1, num_2) {
+    return num_1 >= num_2 
+}
+
+
+console.log(maiorOuIgual(0, 0))
+console.log(maiorOuIgual(0, "0"))
+console.log(maiorOuIgual(5, 1))
+console.log(maiorOuIgual(7, 8))
