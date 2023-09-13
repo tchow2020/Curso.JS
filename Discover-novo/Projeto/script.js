154cf64952214b1a472c18471be3ea4d7efc27ca
tchow2020 <lgmds2017@outlook.com>
2023-09-13 17:42:50 -0300

    Completed-Project

--- Discover-novo/Projeto/script.js
+++ Discover-novo/Projeto/script.js
@@ -1,13 +1,19 @@
 function toggMode(){
-    const html = document.documentElement
     // Verificando se na lista de classes do document html contém a classe light
-    // if(html.classList.contains('light')){
-    //     html.classList.remove('light')
-    // }else{
-    //     html.classList.add('light')
-    // }
-
+    const html = document.documentElement
     html.classList.toggle('light')
+
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    // substituir a imagem
+    if(html.classList.contains('light')){
+        // se tiver o light mode, adicionar a imagem light
+        img.setAttribute('src', './assets/Gojo.gif')
+    }else{
+        // se não tiver o light mode, manter a imagem normal
+        img.setAttribute('src', './assets/vinland-saga.gif')
+    }
 }
 
 toggMode()
